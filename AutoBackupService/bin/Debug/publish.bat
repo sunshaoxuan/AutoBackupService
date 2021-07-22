6de6aec0dd94666052d0efbcb21c27c517b23df5
Parker Sun <ssx@yonyou.com.tw>
2021-07-22 18:21:20 +0800

    remove bin file control.

--- AutoBackupService/bin/Debug/publish.bat
+++
@@ -1,5 +0,0 @@
-net stop AutoBackupService
-del "C:\Program Files\CNDRP.COM\AutoBackupService\*.log"
-del "C:\Program Files\CNDRP.COM\AutoBackupService\AutoBackupService.exe"
-copy "C:\Users\sunsx\Source\Repos\sunshaoxuan\AutoBackupService\AutoBackupService\bin\Debug\AutoBackupService.exe" "C:\Program Files\CNDRP.COM\AutoBackupService\AutoBackupService.exe"
-net start AutoBackupService
